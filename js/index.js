a0eac6ac3ba20c1529b9858104db37aca21ae5f7
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2021-07-05 16:45:11 +0530

    Final Commit

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ const music = document.querySelector("audio");
 const prev = document.getElementById("prev");
 const play = document.getElementById("play");
 const next = document.getElementById("next");
+let progress = document.getElementById("progress");
+let progressDiv = document.getElementById("progressDiv");
+let current_Time = document.getElementById("current_Time");
+let total_duration = document.getElementById("total_duration");
 
 const songs = [
     {
@@ -31,43 +35,78 @@ const songs = [
 
 let isPlaying = false;
 
-const playMusic = ()=> {
+const playMusic = () => {
     isPlaying = true;
     music.play();
     play.classList.replace("fa-play", "fa-pause");
     image.classList.add("animate");
 };
 
-const pauseMusic = ()=> {
+const pauseMusic = () => {
     isPlaying = false;
     music.pause();
     play.classList.replace("fa-pause", "fa-play");
     image.classList.remove("animate");
 };
 
-play.addEventListener("click", ()=> {
+play.addEventListener("click", () => {
     isPlaying ? pauseMusic() : playMusic();
 });
 
 let songIndex = 0;
 
-const loadSong = (songs)=> {
+const loadSong = (songs) => {
     title.innerHTML = songs.title;
     artist.innerHTML = songs.artist;
-    image.src = "images/"+songs.name+".jpg";
-    music.src = "songs/"+songs.name+".mp3";
+    image.src = "images/" + songs.name + ".jpg";
+    music.src = "songs/" + songs.name + ".mp3";
     playMusic();
 };
 
-const nextSong = ()=> {
+const nextSong = () => {
     songIndex = (songIndex + 1) % songs.length;
     loadSong(songs[songIndex]);
 }
 
-const prevSong = ()=> {
+const prevSong = () => {
     songIndex = (songIndex - 1 + songs.length) % songs.length;
     loadSong(songs[songIndex]);
 }
+
+// progress bar logic 
+
+music.addEventListener("timeupdate", (event) => {
+    let { currentTime, duration } = event.srcElement;
+    let progressTime = (currentTime / duration) * 100;
+    progress.style.width = `${progressTime}%`;
+
+    //    total duration logic
+    let min_duration = Math.floor(duration/60);
+    let sec_duration = Math.floor(duration%60);
+    if(sec_duration < 10) {
+        sec_duration = `0${sec_duration}`;
+    }
+    if(duration) {
+        total_duration.innerHTML = `${min_duration}:${sec_duration}`;
+    }
+
+    //    current time logic
+    let min_currentTime = Math.floor(currentTime/60);
+    let sec_currentTime = Math.floor(currentTime%60);
+    if(sec_currentTime < 10) {
+        sec_currentTime = `0${sec_currentTime}`;
+    }
+    current_Time.innerHTML = `${min_currentTime}:${sec_currentTime}`;
+});
+
+progressDiv.addEventListener("click", (event)=> {
+    const { duration } = music;
+    let moveProgress = (event.offsetX / event.srcElement.clientWidth) * duration;
+    music.currentTime = moveProgress;
+    playMusic();
+});
+
+music.addEventListener("ended", nextSong);
 
 next.addEventListener("click", nextSong);
 
